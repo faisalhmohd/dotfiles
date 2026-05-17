f87853b292be10d44f366e3552cdf12790558479
Faisal <faisalhmohd@gmail.com>
2026-05-17 08:44:01 +0600

    fix: defer treesitter load to VeryLazy

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    event = "VeryLazy",
     main = "nvim-treesitter.configs",
     opts = {
         ensure_installed = {
